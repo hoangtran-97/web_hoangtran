ac0cb5f45aa101aae7406bb60f472b7bf9e2b20c
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-10-31 15:27:22 +0200

    Progressive text

--- src/components/banner.js
+++ src/components/banner.js
@@ -7,36 +7,58 @@ import doge from "../images/doge.png";
 import Image from "./image";
 
 const getRandomPoint = (elementWidth, elementHeight) => {
-    const x = `${(Math.random() * 60).toFixed(0)}%`;
-    const y = `${(Math.random() * 75).toFixed(0)}%`;
+    const x = `${(Math.random() * 70).toFixed(0)}%`;
+    const y = `${(Math.random() * 80).toFixed(0)}%`;
     const array = [x, y];
     return array;
 };
 const Banner = () => {
     const [position, setPosition] = useState(["40%", "15%"]);
+    const [dogeRun, setDogeRun] = useState(true);
+    const [title, setTitle] = useState("Well hello there!");
+    const [gameStarted, setGame] = useState(false);
     const animation = useSpring({
         position: "absolute",
         top: position[0],
         right: position[1],
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 450,
+        height: 450,
+        flexDirection: "column"
     });
     return (
         <div style={styles.banner}>
             <div>
-                <h1 style={styles.text}>Well hello there!</h1>
-                <h3 style={styles.text}>I guess you must be very interested in my work to be here</h3>
-                <h3 style={styles.text}>Click on Doge if you want a face reveal!!!</h3>
-                <h3 style={styles.text}>Or you can do what you came here for</h3>
-                <h3 style={styles.text}>And checkout the above sections</h3>
+                <h1 style={styles.text}>{title}</h1>
+                <h3 style={styles.text}>{!gameStarted ? "I guess you must be very interested in my work to be here" : "He loves his PRIVACY."}</h3>
+                <h3 style={styles.text}>{!gameStarted ? "Click on Doge if you want a face reveal!!!" : "You are gonna have to catch him,"}</h3>
+                <h3 style={styles.text}>{!gameStarted ? "Or you can do what you came here for" : "Click on him,"}</h3>
+                <h3 style={styles.text}>{!gameStarted ? "And checkout the above sections" : "Then I'm sure he'll stop"}</h3>
             </div>
             <animated.div
                 style={animation}
                 onMouseEnter={() => {
-                    setPosition(getRandomPoint());
+                    dogeRun
+                        ? setPosition(getRandomPoint()) : null;
+                    gameStarted
+                        ? null
+                        : (
+                            setGame(true),
+                            setTitle("Yeah I forgot to mention,"));
                 }}
-                onMouseLeave={() => {
-                    console.log(getRandomPoint());
+                onClick={() => {
+                    setDogeRun(false);
+                    setPosition(["40%", "15%"]);
                 }}
             >
+                {
+                    gameStarted
+                        ? (<div style={styles.dogeComment}>Get the hell away from me!</div>)
+                        : null
+                }
+
                 <Image />
             </animated.div>
         </div>
@@ -54,6 +76,20 @@ const styles = {
     },
     text: {
         color: colors.gunMetal
+    },
+    dogeComment: {
+        backgroundColor: colors.silverSand,
+        width: 300,
+        height: 40,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        borderBottomRightRadius: 15,
+        position: "absolute",
+        top: -35,
+        left: 200
     }
 };
 export default Banner;
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,5 +1,5 @@
 export default {
     sonicSilver: "#757780",
     gunMetal: "#30323D",
-    silveSand: "#C5C5C5"
+    silverSand: "#C5C5C5"
 };
